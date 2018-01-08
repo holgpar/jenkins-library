48f7c0bb2ca214dcae2f08b94330c06d04fa08aa
Holger Partsch <holger.partsch@sap.com>
2018-01-08 16:14:43 +0100

    Monkey fix

--- src/com/sap/piper/FileUtils.groovy
+++ src/com/sap/piper/FileUtils.groovy
@@ -7,15 +7,10 @@ import java.io.File
 class FileUtils implements Serializable {
 
     static validateDirectory(dir) {
-        if (!dir) throw new IllegalArgumentException("The parameter 'dir' can not be null or empty.")
-        def file = new File(dir)
-        if (!file.exists()) throw new AbortException("'${file.getAbsolutePath()}' does not exist.")
-        if (!file.isDirectory()) throw new AbortException("'${file.getAbsolutePath()}' is not a directory.")
+        retun fileExists(dir)
     }
 
     static validateDirectoryIsNotEmpty(dir) {
-        validateDirectory(dir)
-        def file = new File(dir)
-        if (file.list().size() == 0) throw new AbortException("'${file.getAbsolutePath()}' is empty.")
+        retun fileExists(dir)
     }
 }
